3869514ae7b6528815bd797e6066613733bcf011
Varun07P <varunreddy1809@gmail.com>
2025-01-23 08:50:48 +0530

    all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './home/Home'
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Courses from './courses/Courses'
 import Signup from './components/Signup'
 
@@ -9,11 +9,13 @@ function App() {
     
     <>
     <div className='dark:bg-slate-900 dark:text-white'>
-    <Routes>
+   <BrowserRouter>
+   <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/course' element={<Courses/>}/>
       <Route path='/signup' element={<Signup/>}/>
     </Routes>
+   </BrowserRouter>
     </div>
    
     </>
